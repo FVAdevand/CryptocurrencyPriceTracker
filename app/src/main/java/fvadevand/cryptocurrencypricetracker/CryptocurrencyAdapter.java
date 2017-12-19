60ce2b60c35701e90c90a97d8ac53ad5b3fdb4d6
FVAdevand <pochtauazp@gmail.com>
2017-12-19 20:58:41 +0200

    Add visual polish

--- app/src/main/java/fvadevand/cryptocurrencypricetracker/CryptocurrencyAdapter.java
+++ app/src/main/java/fvadevand/cryptocurrencypricetracker/CryptocurrencyAdapter.java
@@ -11,7 +11,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by Vladimir on 17.12.2017.
@@ -47,11 +49,15 @@ public class CryptocurrencyAdapter extends ArrayAdapter<Cryptocurrency> {
         priceUsdTextView.setText(priceUSD);
 
         TextView percent24hTextView = listItemView.findViewById(R.id.percent24h_text_view);
-        String percent24hString = formatPercent(currentCryptocurrency.getPercentChange24h());
+        double percent24h = currentCryptocurrency.getPercentChange24h();
+        setColor(percent24hTextView, percent24h);
+        String percent24hString = formatPercent(percent24h);
         percent24hTextView.setText(percent24hString);
 
         TextView percent7dTextView = listItemView.findViewById(R.id.percent7d_text_view);
-        String percent7dString = formatPercent(currentCryptocurrency.getPercentChange7d());
+        double percent7d = currentCryptocurrency.getPercentChange7d();
+        setColor(percent7dTextView, percent7d);
+        String percent7dString = formatPercent(percent7d);
         percent7dTextView.setText(percent7dString);
 
         ImageView iconImageView = listItemView.findViewById(R.id.list_item_icon);
@@ -62,12 +68,20 @@ public class CryptocurrencyAdapter extends ArrayAdapter<Cryptocurrency> {
     }
 
     private String formatPrice(double price) {
-        DecimalFormat priceFormat = new DecimalFormat("0.###");
+        DecimalFormat priceFormat = new DecimalFormat("###,###,###.###'$'");
         return priceFormat.format(price);
     }
 
     private String formatPercent(double percent) {
-        DecimalFormat percentFormat = new DecimalFormat("0.0");
+        DecimalFormat percentFormat = new DecimalFormat("0.0#'%'");
         return percentFormat.format(percent);
+    }
+
+    private void setColor(TextView textView, double number) {
+        if (number < 0) {
+            textView.setTextColor(getContext().getResources().getColor(R.color.colorNegativeNumber));
+        } else {
+            textView.setTextColor(getContext().getResources().getColor(R.color.colorPositiveNumber));
+        }
     }
 }
